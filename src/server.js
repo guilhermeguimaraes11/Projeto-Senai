2458b54fc0f74e9b621096dd8565e2c5c19541ec
Guilherme <guilherme10007@gmail.com>
2024-09-09 12:51:13 -0300

    att

--- src/server.js
+++
@@ -1,5 +0,0 @@
-// Importar a instância do Express configurada em index.js
-const app = require("./index");
-
-// Inicia o servidor na porta 5000, neste caso a Api será acessível em http://localhost:5000/projeto_reserva_senai/health/
-app.listen(5000);
