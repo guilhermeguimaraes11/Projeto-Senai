30b9b9ecc15f8d522f2b62bcad5db17fd8feb6cb
Guilherme <guilherme10007@gmail.com>
2024-09-10 15:54:49 -0300

    att

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,5 +1,5 @@
-// Importa o módulo Router do express
-// Router será utilizado para definir rotas específicas da aplicação
-const router = require('express').Router();
+const router = require('express').Router();//importando o módolo express
+
 
 module.exports = router
+//Exportândo a instância de express configurada, para que seja acessada em outros arquivos
